b184894a1206b9f6a95c0aa7a073befdace1e57e
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2026-04-21 14:24:10 -0400

    feat(trips): register Trip and TripStop in Django admin

---
+++ backend/apps/trips/admin.py
@@ -0,0 +1,15 @@
+from django.contrib.gis import admin
+from .models import Trip, TripStop
+
+
+class TripStopInline(admin.TabularInline):
+    model = TripStop
+    extra = 1
+    fields = ('person', 'sequence_order', 'location')
+
+
+@admin.register(Trip)
+class TripAdmin(admin.GISModelAdmin):
+    list_display = ('name', 'date', 'user')
+    list_filter = ('user',)
+    inlines = [TripStopInline]
